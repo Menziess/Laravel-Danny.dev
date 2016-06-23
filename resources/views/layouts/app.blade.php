424c149b07f12f2216882ea9ad70b7f71ddf47a6
Menzies <Stefan_schenk@hotmail.com>
2016-06-23 23:27:06 +0200

    Added progressive support.

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -4,10 +4,15 @@
 	<meta charset="utf-8">
 	<meta http-equiv="X-UA-Compatible" content="IE=edge">
 	<meta name="viewport" content="width=device-width, initial-scale=1, minimum-scale=1, maximum-scale=1">
+	<meta name="theme-color" content="#18bc9c">
 	<meta name="description" content="">
 	<meta name="author" content="">
 
-	<title>Laravel</title>
+	<meta name="mobile-web-app-capable" content="yes">
+	<meta name="apple-mobile-web-app-capable" content="yes">
+	<meta name="apple-mobile-web-app-status-bar-style" content="black-translucent">
+
+	<title>Danny</title>
 
 	<!-- Bootstrap Core CSS - Uses Bootswatch Flatly Theme: http://bootswatch.com/flatly/ -->
 	<link href="{{ asset('css/bootstrap.min.css') }}" rel="stylesheet">
@@ -18,10 +23,10 @@
 	<!-- Custom Fonts -->
 	<link href="{{ asset('font-awesome/css/font-awesome.min.css') }}" rel="stylesheet" type="text/css">
 	<link href="https://fonts.googleapis.com/css?family=Montserrat:400,700" rel="stylesheet" type="text/css">
-	<link href="https://fonts.googleapis.com/css?family=Lato:400,700,400italic,700italic" rel="stylesheet" type="text/css">
 	<link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Lato:100,300,400,700">
 
 	<link rel="stylesheet" href="{{ asset('css/styles.css') }}">
+	<link rel="manifest" href="{{ asset('manifest.json') }}">
 
 	<!-- HTML5 Shim and Respond.js IE8 support of HTML5 elements and media queries -->
 	<!-- WARNING: Respond.js doesn't work if you view the page via file:// -->
@@ -67,8 +72,8 @@
 	<script src="{{ asset('js/cbpAnimatedHeader.js') }}"></script>
 
 	<!-- Contact Form JavaScript -->
-	<script src="{{ asset('js/jqBootstrapValidation.js') }}"></script>
-	<script src="{{ asset('js/contact_me.js') }}"></script>
+	<!-- <script src="{{ asset('js/jqBootstrapValidation.js') }}"></script> -->
+	<!-- <script src="{{ asset('js/contact_me.js') }}"></script> -->
 
 	<!-- Custom Theme JavaScript -->
 	<script src="https://cdnjs.cloudflare.com/ajax/libs/startbootstrap-freelancer/1.0.5/js/freelancer.min.js"></script>
